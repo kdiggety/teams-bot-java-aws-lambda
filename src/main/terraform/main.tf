33941201de1b56d67ba3c83a5225c62a2be8ecf2
kdiggety <wakes-pillory.01@icloud.com>
2025-05-23 20:08:42 -0700

    init

---
+++ src/main/terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "teams_bot_lambda" {
+  function_name = "TeamsBotLambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "com.company.bot.lambda.TeamsBotHandler::handleRequest"
+  runtime       = "java17"
+  timeout       = 30
+  memory_size   = 512
+
+  filename         = var.lambda_jar_path
+  source_code_hash = filebase64sha256(var.lambda_jar_path)
+
+  environment {
+    variables = {
+      TEAMS_BOT_TOKEN = var.teams_bot_token
+    }
+  }
+}
---
+++ src/main/terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "teams_bot_token" {
+  type = string
+}
+
+variable "lambda_jar_path" {
+  type = string
+}
